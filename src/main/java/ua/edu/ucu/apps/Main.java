9d6b8bfd9b0d1c2a8f4efa678030bc5337bf4d00
NazarM <melnyk.pn@ucu.edu.ua>
2024-11-18 22:50:51 +0200

    first commit

---
+++ src/main/java/ua/edu/ucu/apps/Main.java
@@ -0,0 +1,14 @@
+package ua.edu.ucu.apps;
+import ua.edu.ucu.apps.decorator.MockedDocument;
+import ua.edu.ucu.apps.decorator.TimedDocument;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        MockedDocument mockedDocument = new MockedDocument("path");
+        TimedDocument timedDocument = new TimedDocument(mockedDocument);
+        System.out.println(timedDocument.parse());
+    }
+}
